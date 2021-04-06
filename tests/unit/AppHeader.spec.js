7172b0e9590ce59045633becd0eb2798cbfb5ef6
watcharathep <watcharathep@knstrans.com>
2021-04-06 18:01:21 +0700

    first commit

---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,18 @@
+import AppHeader from "@/components/AppHeader.vue";
+import { mount } from "@vue/test-utils";
+
+describe("AppHeader", () => {
+  test("If user is not logged in, do not show logout button", () => {
+    const wrapper = mount(AppHeader);
+    expect(wrapper.find("button").isVisible()).toBeFalsy();
+  });
+
+  test("If user is  logged in, show logout button", async () => {
+    const wrapper = mount(AppHeader);
+    wrapper.setData({
+      loggedIn: true,
+    });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find("button").isVisible()).toBeTruthy();
+  });
+});
